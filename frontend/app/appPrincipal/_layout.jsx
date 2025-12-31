31a3fcc80262c4cf94bc2b669ae5b7f75628a3e1
Rayan <contact.rayanmeri@gmail.com>
2025-12-31 16:55:26 +0100

    Le nombre de produits dans le panier reste en mémoire même si on quitte la boutique

--- frontend/app/appPrincipal/_layout.jsx
+++ frontend/app/appPrincipal/_layout.jsx
@@ -2,21 +2,23 @@ import { Drawer } from "expo-router/drawer";
 import Navbar from "../../components/Navbar";
 import {Platform} from "react-native";
 import {Stack} from "expo-router";
-
+import { PanierProvider } from "../../context/PanierContext";
 
 export default function Layout() {
 
     return (
-        <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="accueil" />
-            <Stack.Screen name="missions" />
-            <Stack.Screen name="social"/>
-            <Stack.Screen name="boutique" />
-            <Stack.Screen name="codebar" />
-            <Stack.Screen name="notifications"/>
-            <Stack.Screen name="parametres"/>
-        </Stack>
-    )
+        <PanierProvider>
+            <Stack screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="accueil" />
+                <Stack.Screen name="missions" />
+                <Stack.Screen name="social" />
+                <Stack.Screen name="boutique" />
+                <Stack.Screen name="codebar" />
+                <Stack.Screen name="notifications" />
+                <Stack.Screen name="parametres" />
+            </Stack>
+        </PanierProvider>
+    );
 
     /*
     (
