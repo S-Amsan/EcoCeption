2a57fb49f0eebfd3ef550110cd0f7e1ed6cb6245
bgdnrvsky <raevskijb@gmail.com>
2026-01-03 13:35:01 +0100

    Add /success endpoint to CompetitionController

--- backend/src/main/java/com/example/backend/controller/CompetitionController.java
+++ backend/src/main/java/com/example/backend/controller/CompetitionController.java
@@ -1,7 +1,10 @@
 package com.example.backend.controller;
 
 import com.example.backend.model.Competition;
+import com.example.backend.model.Success;
 import com.example.backend.repository.CompetitionRepository;
+import com.example.backend.repository.SuccessRepository;
+
 import java.util.Date;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +17,9 @@ public class CompetitionController {
     @Autowired
     private CompetitionRepository competitionRepository;
 
+    @Autowired
+    private SuccessRepository successRepository;
+
     @GetMapping("/all")
     public List<Competition> getAllCompetitions() {
         return competitionRepository.findAll();
@@ -24,5 +30,10 @@ public class CompetitionController {
         return competitionRepository.findFirstByDeadlineAfterOrderByCreationDate(
             new Date()
         );
+    }
+
+    @GetMapping("/success")
+    public List<Success> getAllSuccess() {
+        return successRepository.findAll();
     }
 }
