2a41a38591ce1e32e3dad56b8b0817193d024e19
bgdnrvsky <raevskijb@gmail.com>
2026-01-04 00:14:17 +0100

    Use wildcard import for jakarta.persistence

--- backend/src/main/java/com/example/backend/model/competition/Competition.java
+++ backend/src/main/java/com/example/backend/model/competition/Competition.java
@@ -1,14 +1,7 @@
 package com.example.backend.model.competition;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import java.util.Date;
 import java.util.Set;
 import lombok.Data;
