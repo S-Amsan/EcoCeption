da05752e178af10f671a57d1bbfe26ecd5d8b0af
bgdnrvsky <raevskijb@gmail.com>
2026-01-09 13:51:43 +0100

    Move updating competitoin stats to its service

--- backend/src/main/java/com/example/backend/service/CompetitionService.java
+++ backend/src/main/java/com/example/backend/service/CompetitionService.java
@@ -1,6 +1,8 @@
 package com.example.backend.service;
 
+import com.example.backend.model.User;
 import com.example.backend.model.competition.Competition;
+import com.example.backend.model.competition.CompetitionParticipant;
 import com.example.backend.repository.competition.CompetitionParticipantRepository;
 import com.example.backend.repository.competition.CompetitionRepository;
 import java.util.Date;
@@ -20,5 +22,28 @@ public class CompetitionService {
         return competitionRepository.findFirstByDeadlineAfterOrderByCreationDate(
             new Date()
         );
+    }
+
+    public void updateCurrentCompetitionStats(User user, int diff) {
+        Competition currentCompetition = getLatestCompetition();
+
+        if (currentCompetition == null) {
+            return;
+        }
+
+        var maybeParticipantData =
+            competitionParticipantRepository.findByCompetitionAndUser(
+                currentCompetition,
+                user
+            );
+
+        if (maybeParticipantData.isEmpty()) {
+            return;
+        }
+
+        CompetitionParticipant participantData = maybeParticipantData.get();
+
+        participantData.setPoints(participantData.getPoints() + diff);
+        competitionParticipantRepository.save(participantData);
     }
 }
