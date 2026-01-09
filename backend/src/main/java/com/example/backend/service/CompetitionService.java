30f2a6f855d675209ccabebfe23a606fcf4130e8
bgdnrvsky <raevskijb@gmail.com>
2026-01-09 13:41:41 +0100

    Create `CompetitionService`
    Extract getting the latest competition method

---
+++ backend/src/main/java/com/example/backend/service/CompetitionService.java
@@ -0,0 +1,24 @@
+package com.example.backend.service;
+
+import com.example.backend.model.competition.Competition;
+import com.example.backend.repository.competition.CompetitionParticipantRepository;
+import com.example.backend.repository.competition.CompetitionRepository;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CompetitionService {
+
+    @Autowired
+    private CompetitionRepository competitionRepository;
+
+    @Autowired
+    private CompetitionParticipantRepository competitionParticipantRepository;
+
+    public Competition getLatestCompetition() {
+        return competitionRepository.findFirstByDeadlineAfterOrderByCreationDate(
+            new Date()
+        );
+    }
+}
