87677fcfb784ab02d1f675449828d0dda7e68cc0
Paneed <166396881+Paneed@users.noreply.github.com>
2026-02-19 22:04:12 +0100

    ApiUrl

--- frontend/constants/API_URL.js
+++ frontend/constants/API_URL.js
@@ -2,5 +2,5 @@ import {Platform} from "react-native";
 
 export const API_URL =
     Platform.OS === "android" || Platform.OS === "ios"
-        ? "https://ecoception.fr.nf/api"
-        : "https://ecoception.fr.nf/api";
+        ? "http://ecoception.fr.nf/api"
+        : "http://ecoception.fr.nf/api";
