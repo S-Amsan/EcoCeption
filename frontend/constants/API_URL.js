d8765d884609357b72863476732960e7f08461b9
Paneed <166396881+Paneed@users.noreply.github.com>
2026-02-20 00:00:37 +0100

    Passage en https

--- frontend/constants/API_URL.js
+++ frontend/constants/API_URL.js
@@ -2,5 +2,5 @@ import {Platform} from "react-native";
 
 export const API_URL =
     Platform.OS === "android" || Platform.OS === "ios"
-        ? "http://ecoception.fr.nf"
-        : "http://ecoception.fr.nf";
+        ? "https://ecoception.fr.nf"
+        : "https://ecoception.fr.nf";
